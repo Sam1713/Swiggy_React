401d0d0b246f92d7503c403c24e5424af73f1606
Sam1713 <samsaju399@gmail.com>
2024-04-18 16:42:34 +0530

    Commit message

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,7 +13,7 @@ return(
     <div >
                 <h1 className="m-auto w-2 font-bold my-10">Cart</h1>
         <div className="flex justify-center">
-            {cartItems.length>0?<button onClick={()=>clear()} className="h-8 bg-gradient-to-r from-yellow-300 to-yellow-500 text-white px-3 py-1 rounded-md shadow-md absolute my-10 right-3bg-black text-white mx-20 ">Clear cart</button>:''}
+            {cartItems.length>0?<button onClick={()=>clear()} className="h-8 bg-gradient-to-r from-yellow-300 to-yellow-500  px-3 py-1 rounded-md shadow-md absolute my-10 right-3bg-black text-white mx-20 ">Clear cart</button>:''}
         </div>
         <div className="w-6/12 m-auto my-20">
             {console.log('items',cartItems)}
